4ffa4dd0ce9c68124e7a553f93060610a3d06a61
yunabi21 <yunabi21@gmail.com>
2022-07-15 16:33:16 +0900

    initial commit

---
+++ src/main/java/com/its/library/MysticalLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.its.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysticalLibraryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MysticalLibraryApplication.class, args);
+  }
+
+}
