3163f5a8f344da2b3b6e5d68a291cd1d447243e2
PYSoYa <qkrwnl01@gmail.com>
2022-07-19 16:26:04 +0900

    책, 회차 상세조회

--- src/main/java/com/its/library/repository/EpisodeRepository.java
+++ src/main/java/com/its/library/repository/EpisodeRepository.java
@@ -1,8 +1,13 @@
 package com.its.library.repository;
 
 import com.its.library.entity.EpisodeEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface EpisodeRepository extends JpaRepository<EpisodeEntity, Long> {
 
+    Page<EpisodeEntity> findByBookId(Pageable pageable, Long bookId);
 }
