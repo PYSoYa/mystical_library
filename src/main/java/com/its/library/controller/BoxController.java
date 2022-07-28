453a2a8732db68cf8eb379895e73b7eaa88df1e0
jsb0095 <anfzhd0095@gmail.com>
2022-07-28 09:49:07 +0900

    구매버튼 진행중

--- src/main/java/com/its/library/controller/BoxController.java
+++ src/main/java/com/its/library/controller/BoxController.java
@@ -1,28 +1,32 @@
 package com.its.library.controller;
 
+import com.its.library.dto.BookDTO;
 import com.its.library.dto.BoxDTO;
+import com.its.library.dto.HistoryDTO;
+import com.its.library.dto.MemberDTO;
 import com.its.library.service.BoxService;
+import com.its.library.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.*;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/box")
 public class BoxController {
     private final BoxService boxService;
+    private final MemberService memberService;
 
     // 포인트 보유 체크
     @PostMapping("/pointCheck")
-    public @ResponseBody String pointCheck(@ModelAttribute BoxDTO boxDTO, Long episodeId){
-        System.out.println("boxDTO = " + boxDTO);
-        System.out.println("episodeId = " + episodeId);
-        String result = boxService.pointCheck(boxDTO.getMemberId(), episodeId);
+    public @ResponseBody String pointCheck(@ModelAttribute BoxDTO boxDTO,
+                                           @ModelAttribute HistoryDTO historyDTO, Long episodeId){
+        System.out.println("historyDTO = " + historyDTO);
+        String result = boxService.pointCheck(boxDTO,historyDTO, episodeId);
         System.out.println("result = " + result);
         return result;
     }
@@ -32,5 +36,15 @@ public class BoxController {
     public @ResponseBody String save(@ModelAttribute BoxDTO boxDTO) {
         String result = boxService.save(boxDTO);
         return result;
+    }
+
+    // 구매한 책 출력
+    @GetMapping("/{id}")
+    public String list(@PathVariable("id") Long id, Model model){
+        List<BookDTO> bookDTOList = boxService.list(id);
+        MemberDTO memberDTO = memberService.myPage(id);
+        model.addAttribute("member", memberDTO);
+        model.addAttribute("bookList", bookDTOList);
+        return "member/boxBought";
     }
 }
