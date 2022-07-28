c7595a16742e545a8433a6e299113fe779778109
PYSoYa <qkrwnl01@gmail.com>
2022-07-28 15:44:48 +0900

    열람내역 숨기기 완료

--- src/main/java/com/its/library/repository/HistoryRepository.java
+++ src/main/java/com/its/library/repository/HistoryRepository.java
@@ -5,14 +5,22 @@ import com.its.library.entity.HistoryEntity;
 import com.its.library.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface HistoryRepository extends JpaRepository<HistoryEntity, Long> {
-    List<HistoryEntity> findByMemberEntity(MemberEntity memberEntity);
 
-    List<HistoryEntity> findByEpisodeEntity(EpisodeEntity episodeEntity);
+//    List<HistoryEntity> findByEpisodeEntity(EpisodeEntity episodeEntity);
+
+//    Optional<HistoryEntity> findByBookId(Long bookId);
 
     List<HistoryEntity> findByBooKId(Long bookId);
+
+    @Query(value = "select distinct(h.booKId) from HistoryEntity h where h.memberEntity.id = :memberId")
+    List<Long> findByMemberId(@Param("memberId") Long memberId);
+
+    @Query(value = "select h.episodeEntity.id from HistoryEntity h where h.booKId = :bookId")
+    List<Long> findByEpisodeId(@Param("bookId") Long bookId);
 }
