f865ee0993bedbc639146b5d17087a30fba07bc8
PYSoYa <qkrwnl01@gmail.com>
2022-08-11 09:15:44 +0900

    책 상세화면 페이징 처리 완료

--- src/main/java/com/its/library/common/PagingConst.java
+++ src/main/java/com/its/library/common/PagingConst.java
@@ -2,6 +2,6 @@ package com.its.library.common;
 
 public class PagingConst {
 
-    public static final int PAGE_LIMIT = 10;
+    public static final int PAGE_LIMIT = 5;
     public static final int BLOCK_LIMIT = 3;
 }
