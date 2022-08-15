334c9ee6a37f9d880d59603a9e2b42ae70e93090
yunabi21 <yunabi21@gmail.com>
2022-08-15 21:14:13 +0900

    다크모드 (viewer only) 추가

--- src/main/resources/static/js/episodeDetail.js
+++ src/main/resources/static/js/episodeDetail.js
@@ -5,12 +5,15 @@ window.onload = () => {
   const listNav = document.querySelector('.quick-list-nav');
   const optionButton = document.querySelector('.view-option-wrap');
   const optionPanel = document.querySelector('.option-panel');
+  const darkButton = document.querySelector('.dark-mode-btn-wrap');
 
   document.addEventListener('scroll', function () {
     if (document.documentElement.scrollTop === 0) {
       topButton.className = '';
+      darkButton.classList.remove('on');
     } else {
       topButton.className = 'on';
+      darkButton.classList.add('on');
     }
   });
 
@@ -51,4 +54,48 @@ window.onload = () => {
     per.style.left = percentage + '%';
     per.innerText = Math.floor(percentage) + '%';
   });
+
+  document.getElementById('toggle').addEventListener("click", e => {
+    if (document.body.className === '') {
+      document.body.classList.add("dark-mode");
+
+      document.querySelector('.viewer').classList.add('dark');
+      document.querySelector('.content').classList.add('dark');
+      document.querySelector('.content-footer').classList.add('dark');
+      if (document.querySelector('.star-desc') != null) {
+        document.querySelector('.star-desc').classList.add('dark');
+      }
+      if (document.querySelector('.rate-desc') != null) {
+        document.querySelector('.rate-desc').classList.add('dark');
+      }
+      if (document.querySelector('.edit-btn') != null) {
+        document.querySelector('.edit-btn').classList.add('dark');
+      }
+      if (document.querySelector('.delete-btn') != null) {
+        document.querySelector('.delete-btn').classList.add('dark');
+      }
+    } else {
+      document.body.classList.remove("dark-mode");
+
+      document.querySelector('.viewer').classList.remove('dark');
+      document.querySelector('.content').classList.remove('dark');
+      document.querySelector('.content-footer').classList.remove('dark');
+      document.querySelector('.star-desc').classList.remove('dark');
+      if (document.querySelector('.star-btn') != null) {
+        document.querySelector('.star-btn').classList.remove('dark');
+      }
+      if (document.querySelector('.rate-desc') != null) {
+        document.querySelector('.rate-desc').classList.remove('dark');
+      }
+      if (document.querySelector('.edit-btn') != null) {
+        document.querySelector('.edit-btn').classList.remove('dark');
+      }
+      if (document.querySelector('.delete-btn') != null) {
+        document.querySelector('.delete-btn').classList.remove('dark');
+      }
+    }
+  },false);
+
+  if (document.querySelector('body').className === 'dark-mode') {
+  }
 }
